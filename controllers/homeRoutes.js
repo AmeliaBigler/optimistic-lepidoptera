5eb73ae2de969ace1f30988dd2ca81ba994d9a9c
Amelia Bigler <ameliabigler@Amelias-Air.augiemagic.home>
2023-03-21 15:14:55 -0500

    add handlebars. add login/logout js. add pseudocode for submit article and comment. update homeRoutes.

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,18 +1,17 @@
 const router = require('express').Router();
-const { User } = require('../models');
+const { Article } = require('../models');
 const withAuth = require('../utils/auth');
 
 router.get('/', withAuth, async (req, res) => {
   try {
-    const userData = await User.findAll({
-      attributes: { exclude: ['password'] },
-      order: [['name', 'ASC']],
-    });
+    const articleData = await Article.findAll();
 
-    const users = userData.map((project) => project.get({ plain: true }));
+    const articles = articleData.map((project) => project.get({ plain: true }));
 
+    // first argument is the handlebars that will be rendered
+    // second argument is an object containing what is available to handlebars
     res.render('homepage', {
-      users,
+      articles,
       logged_in: req.session.logged_in,
     });
   } catch (err) {

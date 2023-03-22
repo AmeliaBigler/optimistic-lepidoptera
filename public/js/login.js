e368699eac35f75343f0bd32f15b2bc678966721
Amelia Bigler <ameliabigler@Amelias-Air.augiemagic.home>
2023-03-22 11:49:06 -0500

    add sign up functionality.

--- public/js/login.js
+++ public/js/login.js
@@ -18,5 +18,16 @@ const loginFormHandler = async (event) => {
       }
     }
 };
+
+const loginButton = async () => {
+  document.location.replace('/login');
+};
+
+const signUpButton = async () => {
+  document.location.replace('/signup');
+};
   
-document.querySelector('.login-form').addEventListener('submit', loginFormHandler);
+// document.querySelector('.login-form').addEventListener('submit', loginFormHandler);
+document.querySelector('#login').addEventListener('click', loginButton);
+document.querySelector('#signup').addEventListener('click', signUpButton);
+// document.querySelector('#signUpBtn').addEventListener('click', signUpButton);

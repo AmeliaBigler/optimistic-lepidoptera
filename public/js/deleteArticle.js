34d100c462abc407c1c0ae045600bdaceacfb0cd
Amelia Bigler <ameliabigler@Amelias-Air.augiemagic.home>
2023-03-24 21:18:03 -0500

    git issue. merge files.

--- public/js/deleteArticle.js
+++ public/js/deleteArticle.js
@@ -1,20 +1,24 @@
-const deleteArticleHandler = async (event) => {
-    event.preventDefault();
+const deleteArticleHandler = async (element) => {
   
-    const article_id = document.querySelector('.articleTitle').getAttribute('id');
-  
-    if (article_id) {
-      const response = await fetch(`/api/article/${article_id}`, {
-        method: 'DELETE',
-      });
-  
-      if (response.ok) {
-        document.location.replace('/');
-        console.log()
-      } else {
-        alert('Failed to delete');
-      }
+  const article_id = element.getAttribute('id');
+
+  if (article_id) {
+    const response = await fetch(`/api/article/${article_id}`, {
+      method: 'DELETE',
+    });
+
+    if (response.ok) {
+      document.location.replace('/dashboard');
+    } else {
+      alert('Failed to delete');
     }
+  }
 };
-  
-document.querySelector('#delete').addEventListener('click', deleteArticleHandler);
+
+var deleteBtns = document.querySelectorAll('.delete');
+
+deleteBtns.forEach(function(element) {
+element.addEventListener("click", () => {
+    deleteArticleHandler(element);
+})
+});
